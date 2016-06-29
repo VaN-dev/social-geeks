42d7caa0716fe29e11271befe4aac8e7b237eda0
David Micheau <fanel.dev@gmail.com>
2016-06-29 07:54:33 +0200

    css comments

--- src/Van/FeedBundle/Form/Type/CommentType.php
+++ src/Van/FeedBundle/Form/Type/CommentType.php
@@ -30,7 +30,7 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content', null, [
+            ->add('content', 'text', [
                 'attr' => [
                     'placeholder' => 'Comment this post...',
                 ],
