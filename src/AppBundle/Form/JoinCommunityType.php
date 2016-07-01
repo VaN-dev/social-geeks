1fca394ef90768c1b05cd15323368dc2c769f53a
David Micheau <fanel.dev@gmail.com>
2016-07-01 07:30:25 +0200

    fixing select2

--- src/AppBundle/Form/JoinCommunityType.php
+++ src/AppBundle/Form/JoinCommunityType.php
@@ -3,7 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +12,7 @@ class JoinCommunityType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add("community", TextType::class, [
-                "attr" => [
-                    "placeholder" => "Search a community",
-                ],
+            ->add("community", ChoiceType::class, [
             ])
         ;
     }
